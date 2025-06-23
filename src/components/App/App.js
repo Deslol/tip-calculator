b8adfedbdb99535ad8af0deebe892450702108d8
Cyril Chan <kwunming0911@gmail.com>
2025-06-23 22:50:55 +0100

    Adding select and options

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,37 @@ import './App.css';
 import TextAndInput from "../TextAndInput/TextAndInput";
 import Summary from "../Summary/Summary";
 import ReusableBtn from "../ReusableBtn/ReusableBtn";
+import {useState} from "react";
 
 function App() {
+    const [bill, setBill] = useState(0)
+    const [tip, setTip] = useState(0)
+
+    const options = [
+        {
+            text: 'Dissatisfied',
+            percent: 0
+        },
+        {
+            text: 'It was okay',
+            percent: 0.05
+        },
+        {
+            text: 'It was good',
+            percent: 0.1
+        },
+        {
+            text: 'Absolutely Amazing!',
+            percent: 0.2
+        },
+    ]
+
     return (
         <>
             <div>
                 <TextAndInput>How much was the bill?</TextAndInput>
-                <TextAndInput>How did you like the service?</TextAndInput>
-                <TextAndInput>How did your friend like the service?</TextAndInput>
+                <TextAndInput inputType="select" options={options}>How did you like the service?</TextAndInput>
+                <TextAndInput inputType="select" options={options}>How did your friend like the service?</TextAndInput>
             </div>
             <Summary/>
             <ReusableBtn>Reset</ReusableBtn>
--- src/components/TextAndInput/TextAndInput.jsx
+++ src/components/TextAndInput/TextAndInput.jsx
@@ -1,8 +1,12 @@
 import styles from './TextAndInput.module.css'
-export default function TextAndInput({children, inputType}) {
+
+export default function TextAndInput({children, inputType, options = []}) {
     return (
         <div className={styles.inputContainer}>
             <p>{children}</p>
-            <input type={inputType}/>
+            {inputType === "select" ?
+                <select>{options.map(option => <option
+                    value={option.percent}>{option.text} ({option.percent * 100}%)</option>)}</select> :
+                <input type={inputType}/>}
         </div>)
 }
